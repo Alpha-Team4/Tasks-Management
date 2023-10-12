545eabe823f58c2421d56068b4a54057a6d22f56
kafaldzhievakaterina <katerina.kafaldzhieva@gmail.com>
2023-10-12 14:26:53 +0300

    Implemented Team and ITeam

---
+++ Tasks-Management/Models/Contracts/ITeam.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksManagement.Models.Contracts;
+public interface ITeam
+{
+    string Name { get; }
+
+    IList<IMember> Members { get; }
+
+    IList<IBoard> Borders { get; }
+
+}
--- Tasks-Management/Models/Team.cs
+++ Tasks-Management/Models/Team.cs
@@ -3,8 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TasksManagement.Models.Contracts;
 
 namespace TasksManagement.Models;
-internal class Team
+public class Team : ITeam
 {
+    private const int TeamNameMinLength = 5;
+    private const int TeamNameMaxLength = 15;
+    private const string TeamNameErrorMessage = "Team name must be between {0} and {1} characters.";
+
+    private string name;
+    private readonly IList<IMember> members = new List<IMember>();
+    private readonly IList<IBoard> boards = new List<IBoard>();
+
+    public Team(string name)
+    {
+        this.Name = name;
+    }
+
+    public string Name
+    {
+        get => this.name;
+        private set =>
+            Validator.ValidateStringLength(value,TeamNameMinLength, TeamNameMaxLength, 
+                string.Format(TeamNameErrorMessage, TeamNameMinLength, TeamNameMaxLength));
+    }
+
+    public IList<IMember> Members
+    {
+        get => new List<IMember>(members);
+
+    }
+
+    public IList<IBoard> Borders
+    {
+        get => new List<IBoard>(boards);
+    }
+
+    public void AddMember(IMember member)
+    {
+        this.members.Add(member);
+    }
+
+    public void AddBoard(IBoard board)
+    {
+        this.boards.Add(board);
+    }
 }
