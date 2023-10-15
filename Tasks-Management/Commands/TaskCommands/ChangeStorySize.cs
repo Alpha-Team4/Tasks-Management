872226cb5eb01805d49cb6da3b85e2d29e134383
alexkalionski <alexkalionski@gmail.com>
2023-10-15 22:22:47 +0300

    Implemented ChangeStorySize command.

--- Tasks-Management/Commands/TaskCommands/ChangeStorySize.cs
+++ Tasks-Management/Commands/TaskCommands/ChangeStorySize.cs
@@ -1,10 +1,13 @@
 ﻿using TasksManagement.Commands.Abstracts;
 using TasksManagement.Core.Contracts;
+using TasksManagement.Exceptions;
+using TasksManagement.Models.Contracts;
+using TasksManagement.Models.Enums;
 
 namespace TasksManagement.Commands.TaskCommands;
 public class ChangeStorySize : BaseCommand
 {
-    public const int ExpectedNumberOfArguments = 4;
+    public const int ExpectedNumberOfArguments = 2;
 
     public ChangeStorySize(IList<string> commandParameters, IRepository repository) : base(commandParameters, repository)
     {
@@ -12,6 +15,24 @@ public class ChangeStorySize : BaseCommand
 
     public override string Execute()
     {
-        
+        if (CommandParameters.Count != ExpectedNumberOfArguments)
+        {
+            throw new InvalidUserInputException
+               ($"Invalid number of arguments. Expected: {ExpectedNumberOfArguments} Received: {CommandParameters.Count}");
+        }
+        IStory story = (IStory)Repository.FindTaskByTitle(CommandParameters[0]);
+        story.Size = ParseSize(CommandParameters[1]);
+
+        return $"Story {story.Title} size changed to {story.Size}";
+
+    }
+
+    protected Size ParseSize(string value)
+    {
+        if (Enum.TryParse(value, true, out Size result))
+        {
+            return result;
+        }
+        throw new ArgumentException($"None of the enums in Size match the value {value}.");
     }
 }
