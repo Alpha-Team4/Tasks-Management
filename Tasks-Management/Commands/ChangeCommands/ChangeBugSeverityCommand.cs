70696006f3b813f81c7a0c05967512aa14c60355
alexkalionski <alexkalionski@gmail.com>
2023-10-16 17:52:42 +0300

    Implemented ChangeBugSeverityCommand.

--- Tasks-Management/Commands/ChangeCommands/ChangeBugSeverityCommand.cs
+++ Tasks-Management/Commands/ChangeCommands/ChangeBugSeverityCommand.cs
@@ -1,4 +1,44 @@
-﻿namespace TasksManagement.Commands.ChangeCommands;
-internal class ChangeBugSeverityCommand
+﻿using TasksManagement.Core.Contracts;
+using TasksManagement.Core;
+using TasksManagement.Exceptions;
+using TasksManagement.Models.Contracts;
+using TasksManagement.Commands.Abstracts;
+using TasksManagement.Models.Enums;
+
+namespace TasksManagement.Commands.ChangeCommands;
+public class ChangeBugSeverityCommand : BaseCommand
 {
+    public const int ExpectedNumberOfArguments = 2;
+
+    public ChangeBugSeverityCommand(IList<string> commandParameters, IRepository repository) : base(commandParameters, repository)
+    {
+    }
+
+    public override string Execute()
+    {
+        if (CommandParameters.Count != ExpectedNumberOfArguments)
+        {
+            throw new InvalidUserInputException
+               ($"Invalid number of arguments. Expected: {ExpectedNumberOfArguments} Received: {CommandParameters.Count}");
+        }
+
+        IBug bug = (IBug)Repository.FindTaskByTitle(CommandParameters[0]);
+        //IStory story = (IStory)Repository.FindTaskByTitle(CommandParameters[0]);
+        //IStory story = (IStory)Repository.FindTaskByTitle(CommandParameters[0]);
+
+
+        bug.Severity = ParseSeverity(CommandParameters[1]);
+
+        return $"Story {bug.Title} size changed to {bug.Severity}";
+
+    }
+
+    protected Severity ParseSeverity(string value)
+    {
+        if (Enum.TryParse(value, true, out Severity result))
+        {
+            return result;
+        }
+        throw new ArgumentException($"None of the enums in Severity match the value {value}.");
+    }
 }
