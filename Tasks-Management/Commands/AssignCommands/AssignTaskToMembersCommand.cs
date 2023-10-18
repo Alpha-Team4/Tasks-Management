e7836be77b900206de3e2d0f36f5a285a2698223
alexkalionski <alexkalionski@gmail.com>
2023-10-18 17:28:23 +0300

    Implemented AssignTaskToMemberCommand.

--- Tasks-Management/Commands/AssignCommands/AssignTaskToMembersCommand.cs
+++ Tasks-Management/Commands/AssignCommands/AssignTaskToMembersCommand.cs
@@ -1,4 +1,39 @@
-﻿namespace TasksManagement.Commands.AssignCommands;
-internal class AssignTaskToMembersCommand
+﻿using TasksManagement.Commands.Abstracts;
+using TasksManagement.Core.Contracts;
+using TasksManagement.Exceptions;
+using TasksManagement.Models.Contracts;
+
+namespace TasksManagement.Commands.AssignCommands;
+public class AssignTaskToMembersCommand : BaseCommand
 {
+    private const int ExpectedNumberOfArguments = 4;
+    private const string TaskAlreadyAssignedErrorMessage = "Invalid input. Task {0} is already assigned to member {1}.";
+    private const string TaskAddedToMemberMessage = "Task {0} assigned to member {1}.";
+    public AssignTaskToMembersCommand(IList<string> commandParameters, IRepository repository) 
+        : base(commandParameters, repository)
+    {
+    }
+
+    public override string Execute()
+    {
+        if (CommandParameters.Count < ExpectedNumberOfArguments)
+        {
+            throw new InvalidUserInputException($"Invalid number of arguments. " +
+                $"Expected: {ExpectedNumberOfArguments}, Received: {CommandParameters.Count}");
+        }
+
+        var team = Repository.FindTeamByName(CommandParameters[0]);
+        var board = Repository.FindBoardByName(CommandParameters[1], team);
+        var task = Repository.FindTaskByTitle<ITask>(CommandParameters[2], board);
+        var member = Repository.FindMemberByName(CommandParameters[3]);
+        
+        if (member.Tasks.Contains(task))
+        {
+            throw new InvalidUserInputException(string.Format(TaskAlreadyAssignedErrorMessage, task.Title, member.Name));
+        }
+
+        member.AddTask(task);
+
+        return string.Format(TaskAddedToMemberMessage, task, member); 
+    }
 }
--- Tasks-Management/Commands/ShowCommands/ShowTeamActivityCommand.cs
+++ Tasks-Management/Commands/ShowCommands/ShowTeamActivityCommand.cs
@@ -1,4 +1,37 @@
-﻿namespace TasksManagement.Commands.ShowCommands;
-internal class ShowTeamActivityCommand
+﻿using TasksManagement.Core.Contracts;
+using TasksManagement.Core;
+using TasksManagement.Exceptions;
+using TasksManagement.Commands.Abstracts;
+
+namespace TasksManagement.Commands.ShowCommands;
+public class ShowTeamActivityCommand : BaseCommand
 {
+    private const int ExpectedNumberOfArguments = 1;
+    private const string NoTeamActivityFoundMessage = "The team {0} has no activity yet.";
+
+    public ShowTeamActivityCommand(IList<string> commandParameters, IRepository repository)
+        : base(commandParameters, repository)
+    {
+    }
+
+    public override string Execute()
+    {
+        if (CommandParameters.Count != ExpectedNumberOfArguments)
+        {
+            throw new InvalidUserInputException
+            ($"Invalid number of arguments. Expected: {ExpectedNumberOfArguments}" +
+             $", Received: {CommandParameters.Count}");
+        }
+
+        var team = Repository.FindTeamByName(CommandParameters[0]);
+
+        if (!team.History.Any())
+        {
+            throw new ArgumentException
+                (string.Format(NoTeamActivityFoundMessage));
+        }
+
+        return string.Join(Environment.NewLine,
+            team.History.OrderBy(evt => evt.Time).Select(evt => evt.ToString()));
+    }
 }
