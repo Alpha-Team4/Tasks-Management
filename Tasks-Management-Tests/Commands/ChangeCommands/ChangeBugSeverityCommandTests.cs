04e2743a90ea958dfc2559d2e5a4ae602f4e10b3
George Halachev <george.halachev@gmail.com>
2023-10-18 12:06:55 +0300

    Implement ChangeBugSeverity command Tests

--- Tasks-Management-Tests/Commands/ChangeCommands/ChangeBugSeverityCommandTests.cs
+++ Tasks-Management-Tests/Commands/ChangeCommands/ChangeBugSeverityCommandTests.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TasksManagement.Commands.ChangeCommands;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TasksManagement.Core;
+using TasksManagement.Exceptions;
+using TasksManagement.Models.Enums;
+using static TasksManagement_Tests.Helpers.TestData;
 
 namespace TasksManagement.Commands.ChangeCommands.Tests;
 
@@ -12,14 +10,105 @@ namespace TasksManagement.Commands.ChangeCommands.Tests;
 public class ChangeBugSeverityCommandTests
 {
     [TestMethod]
-    public void ChangeBugSeverityCommandTest()
+    public void Constructor_InitializesCommand()
     {
-        Assert.Fail();
+        var testRepo = new Repository();
+        var testParams = new List<string>();
+
+        var sut = new ChangeBugSeverityCommand(testParams, testRepo);
+
+        Assert.IsInstanceOfType(sut, typeof(ChangeBugSeverityCommand));
     }
 
-    [TestMethod()]
-    public void ExecuteTest()
+    [TestMethod]
+    public void Execute_ChangesBugSeverity()
     {
-        Assert.Fail();
+        // arrange
+        var testRepo = new Repository();
+
+        testRepo.CreateTeam(TeamData.ValidName);
+        testRepo.CreateBoard(BoardData.ValidName, TeamData.ValidName);
+        testRepo.CreateBug(
+            TaskData.ValidTitle, TaskData.ValidDescription, TeamData.ValidName, BoardData.ValidName
+            );
+        
+        var testParams = new List<string> {
+            TeamData.ValidName,
+            BoardData.ValidName,
+            TaskData.ValidTitle,
+            "Critical"
+        };
+        
+        var expectedOutput = $"Bug '{TaskData.ValidTitle}' severity changed to '{Severity.Critical}'.";
+
+        //act
+        var sut = new ChangeBugSeverityCommand(testParams, testRepo);
+
+        Assert.AreEqual(expectedOutput, sut.Execute());
+    }
+
+    [TestMethod]
+    public void Execute_Throws_When_SeverityIsTheSame()
+    {
+        // arrange
+        var testRepo = new Repository();
+
+        testRepo.CreateTeam(TeamData.ValidName);
+        testRepo.CreateBoard(BoardData.ValidName, TeamData.ValidName);
+        testRepo.CreateBug(
+            TaskData.ValidTitle, TaskData.ValidDescription, TeamData.ValidName, BoardData.ValidName
+            );
+        
+        var testParams = new List<string> {
+            TeamData.ValidName,
+            BoardData.ValidName,
+            TaskData.ValidTitle,
+            "0"
+        };
+        
+        var expectedOutput = $"Bug '{TaskData.ValidTitle}' severity changed to '{Severity.Minor}'.";
+
+        //act
+        var sut = new ChangeBugSeverityCommand(testParams, testRepo);
+
+        Assert.ThrowsException<ArgumentException>(() => sut.Execute());
+    }
+
+    [TestMethod]
+    public void Execute_Throws_When_SeverityCannotBeParsed()
+    {
+        // arrange
+        var testRepo = new Repository();
+
+        testRepo.CreateTeam(TeamData.ValidName);
+        testRepo.CreateBoard(BoardData.ValidName, TeamData.ValidName);
+        testRepo.CreateBug(
+            TaskData.ValidTitle, TaskData.ValidDescription, TeamData.ValidName, BoardData.ValidName
+            );
+        
+        var testParams = new List<string> {
+            TeamData.ValidName,
+            BoardData.ValidName,
+            TaskData.ValidTitle,
+            "wrongSeverity"
+        };
+        
+        var expectedOutput = $"Bug '{TaskData.ValidTitle}' severity changed to '{Severity.Minor}'.";
+
+        //act
+        var sut = new ChangeBugSeverityCommand(testParams, testRepo);
+
+        Assert.ThrowsException<InvalidUserInputException>(() => sut.Execute());
+    }
+
+    [TestMethod]
+    public void Execute_Throws_When_ParameterCountIncorrect()
+    {
+        var testRepo = new Repository();
+        var testParams = new List<string> { "testparam", "testparam2", "testparam3" };
+
+        var sut = new ChangeBugSeverityCommand(testParams, testRepo);
+
+        Assert.ThrowsException<InvalidUserInputException>(() => sut.Execute());
     }
 }
