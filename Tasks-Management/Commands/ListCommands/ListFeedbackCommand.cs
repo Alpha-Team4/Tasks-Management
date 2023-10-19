4a804e7c7454b5a0ce9d29afac93c049ee35eef4
alexkalionski <alexkalionski@gmail.com>
2023-10-19 18:06:57 +0300

    Implemented ListFeedbackCommand.

--- Tasks-Management/Commands/ListCommands/ListFeedbackCommand.cs
+++ Tasks-Management/Commands/ListCommands/ListFeedbackCommand.cs
@@ -1,10 +1,17 @@
 ﻿using TasksManagement.Commands.Abstracts;
 using TasksManagement.Core.Contracts;
+using TasksManagement.Exceptions;
 using TasksManagement.Models.Contracts;
+using TasksManagement.Models.Enums;
 
 namespace TasksManagement.Commands.ListCommands;
 public class ListFeedbackCommand : BaseCommand
 {
+    private const int MinExpectedNumberOfArguments = 0;
+    private const int MaxExpectedNumberOfArguments = 1;
+    private const string InvalidFeedbackStatusErrorMessage = "None of the enums in 'StatusFeedback' match the value {0}.";
+    private const string NoFeedbackErrorMessage = "No feedback yet.";
+    private const string NoFeedbackWithStatusErrorMessage = "There are no feedback tasks with the '{0}' status.";
     public ListFeedbackCommand(IList<string> commandParameters, IRepository repository) : base(commandParameters, repository)
     {
 
@@ -13,6 +20,47 @@ public class ListFeedbackCommand : BaseCommand
     public override string Execute()
     {
         var feedback = Repository.FindAllTasks().OfType<IFeedback>().ToList();
+        if (!feedback.Any()) 
+        {
+            throw new EntityNotFoundException(NoFeedbackErrorMessage);
+        }
+        switch(CommandParameters.Count) 
+        {
+            case 0:
+                var orderedFeedback = feedback.OrderBy(feedback => feedback.Title)
+                .ThenBy(feedback => feedback.Rating)
+                .ToList();
 
+                return string.Join(Environment.NewLine, orderedFeedback);
+            case 1:
+                var statusFilter = ParseStatus(CommandParameters[0]);
+                if (!feedback.Any(f => f.Status == statusFilter))
+                {
+                    throw new EntityNotFoundException(NoFeedbackWithStatusErrorMessage);
+                }
+                var filteredFeedback = feedback.Where(feedback => feedback.Status == statusFilter)
+                .OrderBy(feedback => feedback.Title)
+                .ThenBy(feedback => feedback.Rating)
+                .ToList();
+
+                return string.Join(Environment.NewLine, filteredFeedback);
+            default:
+                throw new InvalidUserInputException
+                        ($"Invalid number of arguments. Expected: {MinExpectedNumberOfArguments} - " +
+                         $"{MaxExpectedNumberOfArguments}, Received: {CommandParameters.Count}");
+
+        }
+
+    }
+
+    private StatusFeedback ParseStatus(string value)
+    {
+        if (Enum.TryParse(value, true, out StatusFeedback result))
+        {
+            return result;
+        }
+
+        throw new InvalidUserInputException
+            (string.Format(InvalidFeedbackStatusErrorMessage, value));
     }
 }
