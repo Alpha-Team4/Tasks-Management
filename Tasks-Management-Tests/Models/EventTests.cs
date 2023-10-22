eb5ec3aeb2aad9adbda92a5468f13a0baf8e0823
alexkalionski <alexkalionski@gmail.com>
2023-10-22 17:02:21 +0300

    Implemented Event Test.

---
+++ Tasks-Management-Tests/Models/EventTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TasksManagement_Tests.Helpers.TestData;
+using static TasksManagement_Tests.Helpers.TestHelpers;
+using TasksManagement.Exceptions;
+
+namespace TasksManagement_Tests.Models
+{
+    [TestClass]
+    public class EventTests
+    {
+        [TestMethod]
+        public void Constructor_Should_AssignsCorrectDescription()
+        {
+            var description = "test description";
+            var evt = InitializeTestEvent(description);
+            Assert.AreEqual(evt.Description, description);
+        }
+
+    }
+}
