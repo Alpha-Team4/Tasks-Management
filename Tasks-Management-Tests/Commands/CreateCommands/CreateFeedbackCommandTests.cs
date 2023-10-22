f7b207883bf48b2fdc77d969e54d733e5757bd3a
alexkalionski <alexkalionski@gmail.com>
2023-10-22 18:16:28 +0300

    Implemented CreateStoryCommand and CreateFeedbackCommand tests. Fixed CreateStoryCommand and CreateFeedbackCommand parameter count validation.

---
+++ Tasks-Management-Tests/Commands/CreateCommands/CreateFeedbackCommandTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TasksManagement.Commands.CreateCommands;
+using TasksManagement.Core;
+using TasksManagement.Exceptions;
+using static TasksManagement_Tests.Helpers.TestData;
+using static TasksManagement_Tests.Helpers.TestHelpers;
+
+
+namespace TasksManagement_Tests.Commands.CreateCommands
+{
+    [TestClass]
+    public class CreateFeedbackCommandTests
+    {
+        [TestMethod]
+        public void Constructor_InitializesCommandInstance()
+        {
+            var repository = new Repository();
+
+            var command = new CreateFeedbackCommand(
+                new List<string> { "title", "description", "team", "board" },
+                repository);
+
+            Assert.IsInstanceOfType(command, typeof(CreateFeedbackCommand));
+        }
+
+        [TestMethod]
+        public void Execute_Throws_WhenTooManyParameters()
+        {
+            var repository = new Repository();
+
+            var command = new CreateFeedbackCommand(
+                new List<string> { "title", "description", "team", "board", "other" },
+                repository);
+
+            Assert.ThrowsException<InvalidUserInputException>(
+                () => command.Execute());
+        }
+
+        [TestMethod]
+        public void Execute_ThrowsOn_TooFewParameters()
+        {
+            var repository = new Repository();
+
+            var command = new CreateFeedbackCommand(
+                new List<string> { "title", "description" },
+                repository);
+
+            Assert.ThrowsException<InvalidUserInputException>(
+                () => command.Execute());
+        }
+
+        [TestMethod]
+        public void Execute_WithValidParameters_CreatesFeedback()
+        {
+            ResetTaskLastIssuedIdState();
+            var repository = new Repository();
+            var command = new CreateFeedbackCommand
+                (new List<string> { TaskData.ValidTitle, TaskData.ValidDescription, TeamData.ValidName, BoardData.ValidName }, repository);
+            repository.CreateTeam(TeamData.ValidName);
+            repository.CreateBoard(BoardData.ValidName, TeamData.ValidName);
+
+
+            var result = command.Execute();
+            var expected = "Feedback with ID '1' was created.";
+            var createdFeedback = repository.CreateFeedback
+                (TaskData.ValidTitle, TaskData.ValidDescription, TeamData.ValidName, BoardData.ValidName);
+
+            Assert.AreEqual(expected, result);
+            Assert.IsNotNull(createdFeedback);
+        }
+    }
+}
